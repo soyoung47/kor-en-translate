1a9d8b738b6b2fa3d02ee1d9e7af21d1a565de61
soyoung47 <sy47lee@gmail.com>
2020-12-04 22:05:30 +0900

    Upload seq2seq

---
+++ seq2seq/seq2seq.py
@@ -0,0 +1,248 @@
+import numpy as np
+import re
+import shutil
+import os
+import unicodedata
+import urllib3
+import zipfile
+import time
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Masking
+from tensorflow.keras.models import Model, load_model
+
+# num_samples = 21200
+
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+else:
+    print("Please install GPU version of TF")
+
+def unicode_to_ascii(s):
+  return ''.join(c for c in unicodedata.normalize('NFD', s)
+      if unicodedata.category(c) != 'Mn')
+
+def preprocess_sentence(sent):
+    # 위에서 구현한 함수를 내부적으로 호출
+    sent = unicode_to_ascii(sent.lower())
+
+    # 단어와 구두점 사이에 공백을 만듭니다.
+    # Ex) "he is a boy." => "he is a boy ."
+    sent = re.sub(r"([?.!,])", r" \1", sent)
+
+    sent = re.sub(r"\s+", " ", sent)
+
+    return sent
+
+def load_preprocessed_data():
+    encoder_input, decoder_input, decoder_target = [], [], []
+
+    with open('kor.txt', 'r') as lines:
+        for i, line in enumerate(lines):
+            # source 데이터와 target 데이터 분리
+            src_line, tar_line, _ = line.strip().split('\t')
+            # src_line, tar_line = line.strip().split('\t')
+
+            # source 데이터 전처리
+            src_line_input = [w for w in preprocess_sentence(src_line).split()]
+
+            # target 데이터 전처리
+            tar_line = preprocess_sentence(tar_line)
+            tar_line_input = [w for w in ("<sos> " + tar_line).split()]
+            tar_line_target = [w for w in (tar_line + " <eos>").split()]
+
+            encoder_input.append(src_line_input)
+            decoder_input.append(tar_line_input)
+            decoder_target.append(tar_line_target)
+
+            # # print(i)
+            # if i == num_samples - 1:
+            #     break
+
+    return encoder_input, decoder_input, decoder_target
+
+sents_en_in, sents_kor_in, sents_kor_out = load_preprocessed_data()
+print(sents_en_in[:5])
+print(sents_kor_in[:5])
+print(sents_kor_out[:5])
+
+# 단어 집합 생성
+tokenizer_en = Tokenizer(filters="", lower=False)
+tokenizer_en.fit_on_texts(sents_en_in)
+encoder_input = tokenizer_en.texts_to_sequences(sents_en_in)
+
+# encoding: text -> 정수 (sequence)
+tokenizer_kor = Tokenizer(filters="", lower=False)
+tokenizer_kor.fit_on_texts(sents_kor_in)
+tokenizer_kor.fit_on_texts(sents_kor_out)
+decoder_input = tokenizer_kor.texts_to_sequences(sents_kor_in)
+decoder_target = tokenizer_kor.texts_to_sequences(sents_kor_out)
+
+encoder_input = pad_sequences(encoder_input, padding="post")
+decoder_input = pad_sequences(decoder_input, padding="post")
+decoder_target = pad_sequences(decoder_target, padding="post")
+
+src_vocab_size = len(tokenizer_en.word_index) + 1
+tar_vocab_size = len(tokenizer_kor.word_index) + 1
+print("영어 단어 집합의 크기 : {:d}, 한국어 단어 집합의 크기 : {:d}".format(src_vocab_size, tar_vocab_size))
+
+src_to_index = tokenizer_en.word_index
+index_to_src = tokenizer_en.index_word # 훈련 후 결과 비교할 때 사용
+
+tar_to_index = tokenizer_kor.word_index # 훈련 후 예측 과정에서 사용
+index_to_tar = tokenizer_kor.index_word # 훈련 후 결과 비교할 때 사용
+
+indices = np.arange(encoder_input.shape[0])
+np.random.shuffle(indices)
+print(indices)
+
+encoder_input = encoder_input[indices]
+decoder_input = decoder_input[indices]
+decoder_target = decoder_target[indices]
+
+n_of_val = int(len(indices)*0.1)
+print(n_of_val)
+
+encoder_input_train = encoder_input[:-n_of_val]
+decoder_input_train = decoder_input[:-n_of_val]
+decoder_target_train = decoder_target[:-n_of_val]
+
+encoder_input_test = encoder_input[-n_of_val:]
+decoder_input_test = decoder_input[-n_of_val:]
+decoder_target_test = decoder_target[-n_of_val:]
+
+print(encoder_input_train.shape)
+print(decoder_input_train.shape)
+print(decoder_target_train.shape)
+print(encoder_input_test.shape)
+print(decoder_input_test.shape)
+print(decoder_target_test.shape)
+
+latent_dim = 50
+
+# 인코더
+encoder_inputs = Input(shape=(None,))
+enc_emb =  Embedding(src_vocab_size, latent_dim)(encoder_inputs) # 임베딩 층
+enc_masking = Masking(mask_value=0.0)(enc_emb) # 패딩 0은 연산에서 제외
+encoder_lstm = LSTM(latent_dim, return_state=True) # 상태값 리턴을 위해 return_state는 True
+encoder_outputs, state_h, state_c = encoder_lstm(enc_masking) # 은닉 상태와 셀 상태를 리턴
+encoder_states = [state_h, state_c] # 인코더의 은닉 상태와 셀 상태를 저장
+
+# 디코더
+decoder_inputs = Input(shape=(None,))
+dec_emb_layer = Embedding(tar_vocab_size, latent_dim) # 임베딩 층
+dec_emb = dec_emb_layer(decoder_inputs) # 패딩 0은 연산에서 제외
+dec_masking = Masking(mask_value=0.0)(dec_emb)
+
+# 상태값 리턴을 위해 return_state는 True, 모든 시점에 대해서 단어를 예측하기 위해 return_sequences는 True
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+
+# 인코더의 은닉 상태를 초기 은닉 상태(initial_state)로 사용
+decoder_outputs, _, _ = decoder_lstm(dec_masking,
+                                     initial_state=encoder_states)
+
+# 모든 시점의 결과에 대해서 소프트맥스 함수를 사용한 출력층을 통해 단어 예측
+decoder_dense = Dense(tar_vocab_size, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['acc'])
+
+model.summary()
+
+model.fit(x = [encoder_input_train, decoder_input_train], y = decoder_target_train, \
+          validation_data = ([encoder_input_test, decoder_input_test], decoder_target_test),
+          batch_size = 200, epochs = 10)
+
+# model.save('checkpoint_.h5')
+
+# 테스트 과정을 위한 모델 설계
+encoder_model = Model(encoder_inputs, encoder_states)
+
+# 디코더
+# 이전 시점의 상태를 보관할 텐서
+decoder_state_input_h = Input(shape=(latent_dim,))
+decoder_state_input_c = Input (shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+# 훈련 때 사용했던 임베딩 층을 재사용
+dec_emb2= dec_emb_layer(decoder_inputs)
+
+# 다음 단어 예측을 위해 이전 시점의 상태를 현 시점의 초기 상태로 사용
+decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2, initial_state=decoder_states_inputs)
+decoder_states2 = [state_h2, state_c2]
+
+# 모든 시점에 대해서 단어 예측
+decoder_outputs2 = decoder_dense(decoder_outputs2)
+
+decoder_model = Model(
+    [decoder_inputs] + decoder_states_inputs,
+    [decoder_outputs2] + decoder_states2)
+
+print(time.time())
+
+# 테스트 과정에서의 동작을 위한 함수 구현
+def decode_sequence(input_seq):
+    # 입력으로부터 인코더의 상태를 얻음
+    states_value = encoder_model.predict(input_seq)
+
+    # <SOS>에 해당하는 정수 생성
+    target_seq = np.zeros((1,1))
+    target_seq[0, 0] = tar_to_index['<sos>']
+
+    stop_condition = False
+    decoded_sentence = ''
+
+    # stop_condition이 True가 될 때까지 루프 반복
+    # 구현의 간소화를 위해서 이 함수는 배치 크기를 1로 가정합니다.
+    while not stop_condition:
+        # 이점 시점의 상태 states_value를 현 시점의 초기 상태로 사용
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+
+        # 예측 결과를 단어로 변환
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = index_to_tar[sampled_token_index]
+
+         # 현재 시점의 예측 단어를 예측 문장에 추가
+        decoded_sentence += ' '+sampled_char
+
+        # <eos>에 도달하거나 정해진 길이를 넘으면 중단.
+        if (sampled_char == '<eos>' or
+           len(decoded_sentence) > 50):
+            stop_condition = True
+
+        # 현재 시점의 예측 결과를 다음 시점의 입력으로 사용하기 위해 저장
+        target_seq = np.zeros((1,1))
+        target_seq[0, 0] = sampled_token_index
+
+        # 현재 시점의 상태를 다음 시점의 상태로 사용하기 위해 저장
+        states_value = [h, c]
+
+    return decoded_sentence
+
+# 원문의 정수 시퀀스를 텍스트 시퀀스로 변환
+def seq2src(input_seq):
+    temp=''
+    for i in input_seq:
+        if(i!=0):
+            temp = temp + index_to_src[i]+' '
+    return temp
+
+# 번역문의 정수 시퀀스를 텍스트 시퀀스로 변환
+def seq2tar(input_seq):
+    temp=''
+    for i in input_seq:
+        if((i!=0 and i!=tar_to_index['<sos>']) and i!=tar_to_index['<eos>']):
+            temp = temp + index_to_tar[i] + ' '
+    return temp
+
+for seq_index in [7, 11, 50, 100, 300, 1001]:
+  input_seq = encoder_input_train[seq_index: seq_index + 1]
+  decoded_sentence = decode_sequence(input_seq)
+
+  print("원문 : ",seq2src(encoder_input_train[seq_index]))
+  print("번역문 :",seq2tar(decoder_input_train[seq_index]))
+  print("예측문 :",decoded_sentence[:-5])
+  print("\n")
